abd167293355b9f99f5ed7cc54cb403de7001391
zwang2244 <zwang2244@wisc.edu>
2022-04-03 23:04:52 -0500

    change friend list layout add form placeholder and list for advance query 2

--- movienite/src/Friends/ListOfFriend.js
+++ movienite/src/Friends/ListOfFriend.js
@@ -1,36 +1,32 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
+import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
-import { FixedSizeList } from 'react-window';
-function renderRow(props) {
-  const { index, style } = props;
-
-  return (
-        <ListItem style={style} key={index} component="div" disablePadding>
-        <ListItemButton>
-            <ListItemText primary={`Movie Name ${index + 1}`} />
-            <ListItemText primary={`Vote ${index + 1}`} />
-        </ListItemButton>
-        </ListItem>
-  );
-}
+import ListItemButton from '@mui/material/ListItemButton';
 
 export default function ListOfFriend() {
   return (
-    <Box
-      sx={{ width: '100%', height: 400, maxWidth: 360, bgcolor: 'background.paper' }}
+    <List
+      sx={{
+        width: '100%',
+        maxWidth: 360,
+        bgcolor: 'background.paper',
+        position: 'relative',
+        overflow: 'auto',
+        maxHeight: 300,
+        '& ul': { padding: 0 },
+        m:2
+      }}
+      subheader={<li />}
     >
-      <FixedSizeList
-        height={400}
-        width={360}
-        itemSize={46}
-        itemCount={200}
-        overscanCount={5}
-      >
-        {renderRow}
-      </FixedSizeList>
-    </Box>
+      {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
+        <ListItem key={item}>
+          <ListItemButton>
+            <ListItemText primary={`Movie Name ${item}`} />
+            <ListItemText primary={`Vote ${item}`} />
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </List>
   );
 }
